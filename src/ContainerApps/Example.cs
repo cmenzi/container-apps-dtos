1f67bf5ccedf27a5aeca4be1ef5c451e04ad4d75
Cédric Menzi <cedric.menzi@buhlergroup.com>
2022-01-06 16:50:05 +0100

    example added

---
+++ src/ContainerApps/Example.cs
@@ -0,0 +1,26 @@
+﻿namespace ContainerApps
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Example
+    {
+        public static void Main()
+        {
+            var templateRoot = new ContainerAppTemplateRoot
+            {
+                ResourceGroup = "my-resource-group",
+                Properties = new ContainerAppProperties
+                {
+                    // ...
+                }
+                // ...
+            };
+
+            templateRoot.Save("./deployment.yml");
+
+            // call az containerapps create --yaml <file> ...
+        }
+    }
+}
